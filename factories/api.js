4ebffcec8c09488b88b30dc2ee90771aa58ac89e
Kristen Weber <kristen.weber24@gmail.com>
2017-10-12 11:11:47 -0400

    This is the new Factory

--- factories/api.js
+++ factories/api.js
@@ -4,7 +4,7 @@ app.factory('APIFactory', function($http){
   var baseURL = "http://willshare.com/cs495/MidwestRadioPlayer/";
 
   data.getAllStations = function() {
-    return $http.get(baseURL + 'selectItems.php');
+    return $http.get(baseURL + 'GetAllStations.php');
   }
   return data;
 });
