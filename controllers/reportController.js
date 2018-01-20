4a067aba7ad073a94a89a590d7d6c0381c2b223f
Kristen Weber <kristen.weber24@gmail.com>
2018-01-20 13:15:05 -0500

    started design on report form

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,4 +1,14 @@
 app.controller('reportController', function($scope, APIFactory) {
 
+  APIFactory.getAllStations().then(function (response){
+      $scope.allActiveStations = response.data.active;
+      $scope.activeStations = response.data.active;
+
+      for(var i = 0; i < $scope.activeStations.length; i++){
+          $scope.activeStations[i].edit = true;
+      }
+  });
+
+  $scope.showActive = true;
 
 });
